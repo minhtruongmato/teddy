2dd3cb4551d95238e5c09b11eddd3e8d69dd0f4c
hungluong249 <hung.luong@matocreative.vn>
2018-08-24 14:23:57 +0700

    reconfig source

---
+++ application/controllers/Homepage.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Homepage extends public_Controller {
+
+
+    public function index()
+    {
+        $this->render('homepage_view');
+    }
+}
